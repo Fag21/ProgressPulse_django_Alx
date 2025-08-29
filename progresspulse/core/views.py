9612677ace03e00639c31336173bc421a5a70885
Fag21 <filebaralemu@gmail.com>
2025-08-29 09:31:31 -0700

    deleting unimportan folder

--- progresspulse/core/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-def home(request):
-    return render(request, 'core/home.html')
-
-@login_required
-def dashboard(request):
-    return render(request, 'core/dashboard.html')
--- progresspulse/progresspulse/settings.py
+++ progresspulse/progresspulse/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+from pathlib import Path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,5 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
